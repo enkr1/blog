b25de305ebbd972e8149051d76b2842fac9ef027
Jing Hui PANG (彭竞辉) <enkr99@gmail.com>
2025-04-07 02:34:59 +0800

    Site updated: 2025-04-07 02:34:41

--- js/utils.js
+++ js/utils.js
@@ -16,10 +16,7 @@ NexT.utils = {
       var $image = $(element);
       var imageLink = $image.attr('data-src') || $image.attr('src');
       var $imageWrapLink = $image.wrap(`
-        <a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url">
-          <div class="image-wrapper">
-          </div>
-        </a>
+        <a class="fancybox fancybox.image" href="${imageLink}" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>
         `).parent('a');
       if ($image.is('.post-gallery img')) {
         $imageWrapLink.attr('data-fancybox', 'gallery').attr('rel', 'gallery');
